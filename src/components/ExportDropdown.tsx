53b186c4f5dc20beeb263ee9cc7553ffda2b3ac8
Umer Aalam <umerrazzaq.dev@gmail.com>
2026-02-01 21:11:41 +0500

    dropdown

--- src/components/ExportDropdown.tsx
+++ src/components/ExportDropdown.tsx
@@ -1,12 +1,17 @@
+import { ChevronDown } from 'lucide-react';
+import { useState,type ChangeEvent } from 'react';
+
 interface Props {
   exportFormat: string;
-  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
 }
 const ExportDropdown = ({ exportFormat, onChange }: Props) => {
+  const [open,setOpen] = useState(false);
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex w-full max-w-[500px] justify-center items-center">
       <select
         value={exportFormat}
+        onMouseDown={()=>setOpen(!open)}
         onChange={onChange}
         className="w-full flex appearance-none cursor-pointer rounded-full bg-white px-3 py-2 font-black text-gray-800 dark:bg-gray-900 dark:text-white"
       >
@@ -17,6 +22,7 @@ const ExportDropdown = ({ exportFormat, onChange }: Props) => {
           MP3 (compressed)
         </option>
       </select>
+      <ChevronDown className={`absolute top-auto right-10 transition-all ${open && ""}`} />
     </div>
   );
 };
